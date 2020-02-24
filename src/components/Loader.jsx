474499e4696c016a7ef0593cc01dc407b9cb6b2d
Daniel H <daguiheso@gmail.com>
2020-02-24 13:08:28 -0500

    create Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../assets/styles/components/Loader.css';
+
+const Loader = () => (
+  <div className="lds-grid">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
