1c7696dd62524c82cf4b9e5e56b264f1da064350
Daniel H <daguiheso@gmail.com>
2020-02-24 21:59:19 -0500

    add  Link to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import logo from '../assets/images/badge-header.svg';
 import "../assets/styles/components/Navbar.css";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar-conf">
       <div className="container-fluid">
-        <a href="/" className="navbar-conf__brand">
+        <Link to="/badges" className="navbar-conf__brand">
           <img className="navbar-conf__logo" src={logo} alt="Logo" />
           <span className="font-weight-light">Conference </span>
           <span className="font-weight-bold">Badges</span>
-        </a>
+        </Link>
       </div>
     </nav>
   );
