6387e26fb41ac93f842191c1fe881053b9a4dd27
Daniel H <daguiheso@gmail.com>
2020-02-25 11:19:58 -0500

    add route /badges/:badgeId/edit

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
+import BadgeEdit from '../pages/BadgeEdit';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import Layout from './Layout';
@@ -12,6 +13,7 @@ const App = () => (
       <Switch>
         <Route exact path="/badges" component={Badges} />
         <Route exact path="/badges/new" component={BadgeNew} />
+        <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
         <Route exact path="/home" component={Home} />
         <Route component={NotFound} />
       </Switch>
