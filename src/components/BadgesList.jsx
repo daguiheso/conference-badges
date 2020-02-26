7dadf037982a67b6d589994dbd4397fa902ab8c2
Daniel H <daguiheso@gmail.com>
2020-02-25 22:55:59 -0500

    create filter badges

--- src/components/BadgesList.jsx
+++ src/components/BadgesList.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BadgesListItem from '../components/BadgeListItem';
 import '../assets/styles/components/BadgesList.css';
 
 const BadgesList = (props) => {
+  const [query, setQuery] = useState('');
+  const filteredBadges = props.badges.filter(badge => {
+    return `
+      ${badge.firstName}
+      ${badge.toLowerCase}
+      ${badge.twitter}
+      ${badge.jobTitle}
+    `.toLowerCase()
+     .includes(query)
+  });
+
+  const handleOnChangeQuery = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
     <div className="badges-list">
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input
+          type="text"
+          className="form-control"
+          value={query}
+          onChange={handleOnChangeQuery}
+        />
+      </div>
       <ul className="list-unstyled">
-        { props.badges.map((badge) => {
+        { filteredBadges.map((badge) => {
           return (
             <li key={badge.id}>
               <BadgesListItem
